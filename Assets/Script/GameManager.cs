9803d1ffc35e64c5d0e29d707bf39785737fb96d
AhmetHyseni <ahmetfin4b@gmail.com>
2022-11-07 08:15:16 +0200

    laittanut pelaajat ja score

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject player1;
+    [SerializeField] private int player1Score = 0;
+    [SerializeField] private GameObject player2;
+    [SerializeField] private int player2Score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
