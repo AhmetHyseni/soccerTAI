4ae686960aa87a7d7d4714d3bb4be612cf11c506
rs0642 <salminennrasmus@gmail.com>
2022-11-21 10:32:05 +0200

    Revert "Tunnin loppu tallennus"
    This reverts commit a62e12a724001c629201732c96fe124beeabab1f.

--- Assets/Script/playercontroller.cs
+++ Assets/Script/playercontroller.cs
@@ -5,47 +5,21 @@ using UnityEngine;
 public class playercontroller : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public float speed = 150f;
+    public float speed = 15f;
     bool isGrounded = true;
-    float movementX;
     public float jumpPower = 300f;
     void Start()
     {
-        movementX = 0;
         rb = GetComponent<Rigidbody2D>();
-    }
-    void Update()
-    {
-        player1Input();
     }
     void FixedUpdate()
     {
-        playermovement1();
+        playermovement();
     }
-    void player1Input()
+
+    void playermovement()
     {
-        Debug.Log("MovementX arvo on: "+ movementX);
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            movementX = -1f*speed;
-            
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            movementX = 1f*speed;
-        }
-        if(Input.GetKeyUp(KeyCode.D))
-        {
-            movementX = 0f;
-        }
-        if(Input.GetKeyUp(KeyCode.A))
-        {
-            movementX = 0f;
-        }
-    }
-    void playermovement1()
-    {   
-        rb.velocity = new Vector2(movementX, rb.velocity.y);
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, rb.velocity.y);
 
         if (Input.GetKey(KeyCode.W) && isGrounded==true)
         {
