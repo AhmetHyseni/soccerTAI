8bc9cc4a56d2c7b854209fe8cd5bed03b1fe2c29
AhmetHyseni <ahmetfin4b@gmail.com>
2023-03-20 09:16:48 +0200

    Testi

--- Assets/Script/Ai.cs
+++ Assets/Script/Ai.cs
@@ -14,6 +14,7 @@ public class Ai : MonoBehaviour
     public float jumpForce;
     private float tempTime;
     private bool avoidPlayer = false;
+    private bool ballOnOwnHalf = true;
 
     [Header("Ray Cast")]
     public Vector2 pivotPoint = Vector2.zero;
@@ -23,8 +24,6 @@ public class Ai : MonoBehaviour
     public float angle3;
 
     private Vector2 startPoint = Vector2.zero;
-
-
 
     void Start()
     {
@@ -38,8 +37,6 @@ public class Ai : MonoBehaviour
         Vector2 direction2 = GetDirectionVector2D(angle2);
         Vector2 playerDirection = (ball.position - transform.position).normalized;
 
-
-
         Physics2D.Raycast(startPoint, direction, range);
         Physics2D.Raycast(startPoint, direction2, range);
         Physics2D.Raycast(startPoint, playerDirection, range);
@@ -47,42 +44,52 @@ public class Ai : MonoBehaviour
         Debug.DrawRay(startPoint, direction2 * range, Color.green);
         Debug.DrawRay(startPoint, playerDirection * range, Color.green);
         JumpCooldown();
+
+        // Check if the ball is on the AI's half of the field
+        if (ball.position.x < transform.position.x)
+        {
+            ballOnOwnHalf = true;
+        }
+        else
+        {
+            ballOnOwnHalf = false;
+        }
     }
+
     public Vector2 GetDirectionVector2D(float angle)
     {
         return new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)).normalized;
     }
+
     void FixedUpdate()
     {
         AvoidPlayer();
         NoOwnGoal();
         ballLocation = ball.position.x - transform.position.x;
         float ballLocationY = ball.position.y - transform.position.y;
-        moveLocation = new Vector2(ballLocation, 0);
 
-        if (avoidPlayer == false)
+        // If the ball is on the AI's half of the field, follow it as usual
+        if (ballOnOwnHalf || Mathf.Abs(ballLocation) < 300f)
         {
+            moveLocation = new Vector2(ballLocation, 0);
             rb.AddForce(moveLocation.normalized * speed);
         }
     }
 
     void JumpCooldown()
-    { 
-        
-      if (ball.transform.position.y > transform.position.y)
-      {
-         tempTime += Time.deltaTime;
+    {
+        if (ball.transform.position.y > transform.position.y)
+        {
+            tempTime += Time.deltaTime;
             if (tempTime >= 2f)
             {
                 Vector2 jumpDirection = (ball.transform.position - transform.position).normalized;
                 rb.AddForce(jumpDirection * jumpForce, ForceMode2D.Impulse);
                 tempTime = 0;
             }
-      }
-                       
-        
-        //Debug.Log(tempTime);
+        }
     }
+
     void NoOwnGoal()
     {
         float tempBallLocation = ball.position.x;
@@ -93,6 +100,7 @@ public class Ai : MonoBehaviour
             rb.AddForce(Vector2.right * speed);
         }
     }
+
     void AvoidPlayer()
     {
         if (p1.transform.position.x > transform.position.x && ball.transform.position.x > transform.position.x)
@@ -100,16 +108,13 @@ public class Ai : MonoBehaviour
             rb.AddForce(Vector2.up * jumpForce);
         }
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Player1")
         {
             avoidPlayer = true;
             Debug.Log("Touching Player 1!");
-        }
-        else
-        {
-            avoidPlayer = false;
         }
     }
 }
